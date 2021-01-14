a190565c302869e74e75fd79f272bc7b7cbf277c
MestrieEsteban <esteban94.em@gmail.com>
2021-01-14 15:30:03 +0100

    v1 fini (jeu fonctionelle)

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,34 @@ const log = console.log;
 class Game {
 	gamePosition: object = {};
 	gameSaveArray: string = "";
+	gameMaxY: number = 0;
+	gameMaxX: number = 0;
 }
 
 async function startGame() {
 	log(chalk.yellow('Welcome to pp-ailumette game !'))
 	let acutalGame = game()
-	log(chalk.blue('_________'))
+	show(acutalGame)
 	while (countMetteAll(acutalGame) >= 1) {
-		let line = readlineSync.question('Chose your y :');
-		let matches = readlineSync.question('Chose your nb :');
+		log(chalk.blue('_________'))
+		log(chalk.blue('Your turn'))
+		let playerLineError: boolean = true
+		let playerMatcheError: boolean = true
+		let line: number
+		let matches: number
+		while (playerLineError) {
+			line = readlineSync.question('Line : ');
+			playerLineError = testLinePlayerError(line, acutalGame)
+		}
+		while (playerMatcheError) {
+			matches = readlineSync.question('Matches : ');
+			playerMatcheError = testMatchesPlayerError(line, matches, acutalGame)
+		}
+		log(chalk.blue(`Player removed ${matches} match(es) from line ${line} `))
 		demiseMette(acutalGame, matches, line)
+		log(chalk.green('_________'))
+		log(chalk.green('IA\'s turn...'))
+		theBestIACreatedEver(acutalGame)
 	}
 	log('finish')
 }
@@ -31,9 +49,15 @@ function game() {
 	for (let line of gameLine) {
 		let x: number = -1;
 		y++;
+		if (game.gameMaxY <= y) {
+			game.gameMaxY = y + 1
+		}
 		if (!line) { break; }
 		for (let value of line.split('')) {
 			x++;
+			if (game.gameMaxX < x) {
+				game.gameMaxX = x
+			}
 			let position: string = `${x}:${y}`;
 			game.gamePosition[position] = {
 				value: value
@@ -57,7 +81,7 @@ function demiseMette(game: Game, nbDemise: number, y: number) {
 	let x: number = 0;
 	let count: number = 0;
 
-	while (x !== 9) {
+	while (x !== game.gameMaxX) {
 		if (game.gamePosition[`${x}:${y}`].value == '|') {
 			if (count != nbDemise) {
 				count++
@@ -72,9 +96,10 @@ function demiseMette(game: Game, nbDemise: number, y: number) {
 
 }
 
-function show(game: Game) {
+async function show(game: Game) {
 
 	let oldY = 0
+	game.gameSaveArray = ""
 	for (let obj in game.gamePosition) {
 		let XandY = obj.split(":");
 		let X = parseInt(XandY[0], 10)
@@ -85,7 +110,7 @@ function show(game: Game) {
 		game.gameSaveArray = game.gameSaveArray + game.gamePosition[obj]['value']
 		oldY = Y
 	}
-	log(chalk.red(game.gameSaveArray))
+	log(chalk.red.bold(game.gameSaveArray))
 	//let a = countMette(game, 3);
 
 }
@@ -93,7 +118,7 @@ function show(game: Game) {
 function countMette(game: Game, y: number): number {
 	let x: number = 0;
 	let count: number = 0
-	while (x !== 9) {
+	while (x !== game.gameMaxX) {
 		if (game.gamePosition[`${x}:${y}`].value == '|') {
 			count++
 		}
@@ -109,6 +134,71 @@ function countMetteAll(game: Game): number {
 		}
 	});
 	return count;
+}
+
+function theBestIACreatedEver(game: Game) {
+	let chosedLine: boolean = true
+	let y:number
+	while (chosedLine) {
+		y = Math.floor(Math.random() * ((game.gameMaxY - 2) - 1 + 1) + 1)
+		if (countMette(game, y) > 0) {
+			chosedLine = false
+		}
+	}
+	let nb = Math.floor(Math.random() * (countMette(game, y) - 0 + 1) + 0)
+
+	log(chalk.green(`IA removed ${nb} match(es) from line ${y} `))
+	demiseMette(game, nb, y)
+}
+
+
+
+
+
+
+
+
+
+function testLinePlayerError(line: number, game: Game) {
+	if (line > game.gameMaxY - 2) {
+		log('Error: this line is out of range')
+		return true
+	}
+	if (line == 0) {
+		log('Error: this line is out of range')
+		return true
+	}
+	if (line < 0) {
+		log('Error: invalid input (positive number expected)')
+		return true
+	}
+	if (line > 0 && line < game.gameMaxY - 1) {
+		return false
+	} else {
+		log('Error: invalid input (positive number expected)')
+		return true
+	}
+}
+
+function testMatchesPlayerError(line: number, matches: number, game: Game) {
+	if (matches == 0) {
+		log('Error: you have to remove at least one match')
+		return true
+	}
+	if (matches < 0) {
+		log('Error: invalid input (positive number expected)')
+		return true
+	}
+	if (matches > countMette(game, line)) {
+		log('Error: not enough matches on this line')
+		return true
+	}
+	if (matches > 0 && line <= countMette(game, line)) {
+		return false
+	} else {
+		log('Error: invalid input (positive number expected)')
+		return true
+	}
 }
 
 startGame();
